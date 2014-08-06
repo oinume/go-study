dded1f07d80696fabc2ca323a44c072e6d835158
oinume <oinume@gmail.com>
2014-08-06 13:24:38 +0900

    メソッドをオーバーライドする時に元のメソッドを呼べるか試してみた

--- method_override/method_override.go
+++ method_override/method_override.go
@@ -7,14 +7,20 @@ import (
 )
 
 func main() {
+	fmt.Println("--- Login() ---")
 	user1 := User{1, "oinume", "test"}
 	result, err := user1.Login()
 	fmt.Printf("%10s: Login() = %v, %v\n", user1.Name, result, err)
 
-	user2 := BannedUser{User{2, "restricted", "test"}}
+	user2 := BannedUser{User{2, "banned", "test"}, "Too many spams"}
 	result2, err2 := user2.Login()
 	fmt.Printf("%10s: Login() = %v, %v\n", user2.Name, result2, err2)
+
+	fmt.Println("--- String() ---")
+	fmt.Println(user1.String())
+	fmt.Println(user2.String())
 }
+
 
 type User struct {
 	Id int
@@ -22,17 +28,27 @@ type User struct {
 	Password string
 }
 
-func (*User) Login() (bool, error) {
+func (user *User) Login() (bool, error) {
 	return true, nil
+}
+
+func (user *User) String() string {
+	return fmt.Sprintf("Id:%d, Name:%s", user.Id, user.Name)
 }
 
 type BannedUser struct {
 	User
+	Reason string
 }
 
-// User.Login()をオーバライド
-func (*BannedUser) Login() (bool, error) {
+// User.Login()をオーバーライド
+func (user *BannedUser) Login() (bool, error) {
 	return false, errors.New("You are banned.")
+}
+
+// User.String()をオーバーライドしつつ中で呼び出す
+func (user *BannedUser) String() string {
+	return user.User.String() + ", Reason:" + user.Reason
 }
 
 //func (*BannedUser) Login() (bool) {
